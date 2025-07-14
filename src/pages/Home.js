4f422965d8197d84e4be193efbe7b21d3bf3ac9a
Cameron Johnson <cjplanes@gmail.com>
2025-07-13 18:34:24 -0700

    Logo changes and updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
 
 function Home() {
   return (
-    <div>
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
       <h1>Home Page</h1>
       <p>Welcome to my website!</p>
     </div>
