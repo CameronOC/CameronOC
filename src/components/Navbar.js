4b0ca1a8bd052f311a9d20b0e1aee33dde61d952
Cameron Johnson <cjplanes@gmail.com>
2025-07-13 18:30:10 -0700

    Updated with vibe code

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/projects" className="nav-link">Projects</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/contact" className="nav-link">Contact Me</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
